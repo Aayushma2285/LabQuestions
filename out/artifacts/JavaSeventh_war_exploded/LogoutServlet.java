431848072b040fb3193607ecd32b66bbbf5e4e09
aayush2285 <raiaayushma798@gmail.com>
2026-04-07 22:10:21 +0545

    JSP Server Files appended

---
+++ out/artifacts/JavaSeventh_war_exploded/LogoutServlet.java
@@ -0,0 +1,19 @@
+package labQuestions.servletAndJSP;
+//commit
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) session.invalidate();
+
+        // Remove cookie
+        Cookie cookie = new Cookie("username", "");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
+        response.sendRedirect("login.jsp");
+    }
+}
